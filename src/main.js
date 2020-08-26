4a15f09f08a06650236e1cb672b72816bb1778f6
xiaohuang1234567 <1049358743@qq.com>
2020-08-26 22:15:55 +0800

    初始化配置2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入通用的样式
+import './styles/base.less'
+import './styles/iconfont.css'
+// 导入vant
+// import Vant from 'vant'
+import { Button } from 'vant'
+// import 'vant/lib/index.css'
+
+// 导入全局组件
+// Vue.use(Vant)
+Vue.use(Button)
+// import Button from 'vant/lib/button'
+// import 'vant/lib/button/style'
 
 Vue.config.productionTip = false
 
